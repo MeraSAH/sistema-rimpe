de9f8eded6babf28346ced22c589da5e1a7200f8
Stalin Mera <stalinmeramk2122@gmail.com>
2026-08-27 15:52:48 -0500

    Rename firebaseConfig to FIREBASE_CONFIG

--- js/firebase.js
+++ js/firebase.js
@@ -17,14 +17,13 @@
 // 🔧 REEMPLAZA con la configuración de tu proyecto Firebase
 // (Firebase Console → ⚙️ Configuración del proyecto → "Tus apps" → SDK setup and configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyB-dROCFBg075fp8bK_eBlZPmJST8Y5YiU",
-  authDomain: "muebleriaycerrajeria-benjamin.firebaseapp.com",
-  projectId: "muebleriaycerrajeria-benjamin",
-  storageBucket: "muebleriaycerrajeria-benjamin.firebasestorage.app",
-  messagingSenderId: "413810060846",
-  appId: "1:413810060846:web:d238436fed3a333e16f9bf",
-  measurementId: "G-TX31YPBRHX"
+const FIREBASE_CONFIG = {
+    apiKey:            "AIzaSyB-dROCFBg075fp8bK_eBlZPmJST8Y5YiU",
+    authDomain:        "muebleriaycerrajeria-benjamin.firebaseapp.com",
+    projectId:         "muebleriaycerrajeria-benjamin",
+    storageBucket:     "muebleriaycerrajeria-benjamin.firebasestorage.app",
+    messagingSenderId: "413810060846",
+    appId:             "1:413810060846:web:d238436fed3a333e16f9bf"
 };
 
 // ─── Cliente ──────────────────────────────────────────────────────────────────
